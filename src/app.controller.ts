c26e60ebc24d7a7d3a7f7502bd358887b42d3781
shukrullojonappdev <shukrullojon.dev@gmail.com>
2021-12-12 19:54:08 +0500

    changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Request, UseGuards, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  UseGuards,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { User } from './users/entities/user.entity';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private authService: AuthService,
+    private usersService: UsersService,
   ) {}
 
   @Get()
@@ -20,6 +31,11 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Post('auth/registration')
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
   }
 
   @UseGuards(JwtAuthGuard)
